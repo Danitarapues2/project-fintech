61fecaa36cb086fd2a25f25956a3c35c56e0d993
Danitarapues2 <dpr.tarapues@yavirac.edu.ec>
2024-02-03 18:16:37 -0500

    Diseño de vista de listar clientes con buscador funcionando

---
+++ src/public/script/cliente/buscador.js
@@ -0,0 +1,30 @@
+
+document.addEventListener('DOMContentLoaded', () => {
+
+    const buscar = document.getElementById('buscar');
+
+    buscar.addEventListener('keyup', () => {
+
+        const texto = buscar.value.toLowerCase();
+
+
+
+        document.querySelectorAll('#table-gestion-clientes tbody tr').forEach(fila => {
+
+            let nombre = fila.querySelector('td:nth-child(1) p').textContent.toLowerCase();
+            let apellido = fila.querySelector('td:nth-child(2) p').textContent.toLowerCase();
+            let cedula = fila.querySelector('td:nth-child(3) p').textContent.toLowerCase();
+            let celular = fila.querySelector('td:nth-child(4) p').textContent.toLowerCase();
+            let correo = fila.querySelector('td:nth-child(5) p').textContent.toLowerCase();
+            let direccion = fila.querySelector('td:nth-child(6) p').textContent.toLowerCase();
+
+            if (!nombre.includes(texto) && !apellido.includes(texto) && !cedula.includes(texto) && !celular.includes(texto) && !correo.includes(texto) && !direccion.includes(texto)) {
+                fila.style.display = 'none';
+            } else {
+                fila.style.display = '';
+            }
+
+        });
+
+    });
+});
