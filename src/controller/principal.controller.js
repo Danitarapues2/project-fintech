2fc17b5c57ec24e1745d1a894e05d6d112f3ff53
jossalajop <102058287+jossalajop@users.noreply.github.com>
2024-02-04 21:56:02 -0500

    modificacion

--- src/controller/principal.controller.js
+++ src/controller/principal.controller.js
@@ -7,4 +7,7 @@ principal.Mostrar = async(req, res) => {
     res.render('tienda/agregar');
 }
 
+//Aqui agregar una funcion que me permita registrar una soloa vez la tienda 
+//Ademas dirrecionarle a la pagina del listado de la tienda
+
 module.exports = principal
