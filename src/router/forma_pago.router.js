90818e4b0bcdb8bab161bc22fab9f076a4519f7b
jossalajop <102058287+jossalajop@users.noreply.github.com>
2024-02-09 22:24:03 -0500

    forma

--- src/router/forma_pago.router.js
+++ src/router/forma_pago.router.js
@@ -3,7 +3,7 @@ const express = require("express");
 const router = express.Router();
 const { mostrar,mandar,listar} = require("../controller/forma_pago.controller");
 
-router.post('/agregar',mostrar);
+router.get('/agregar',mostrar);
 router.post('/agregar',mandar)
 router.get('/listar',listar)
 
